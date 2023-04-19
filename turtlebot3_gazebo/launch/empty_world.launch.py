144cd7dd8b597ebcb7b6760857e434b3c887b371
ChrisTimperley <christimperley@googlemail.com>
2023-04-19 09:38:13 -0400

    added gui condition to empty_world.launch.py

--- turtlebot3_gazebo/launch/empty_world.launch.py
+++ turtlebot3_gazebo/launch/empty_world.launch.py
@@ -21,6 +21,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
@@ -32,6 +33,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
+
+    gui = DeclareLaunchArgument('gui', default_value='True')
 
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
@@ -49,7 +52,8 @@ def generate_launch_description():
     gzclient_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
+        ),
+        condition=IfCondition(gui),
     )
 
     robot_state_publisher_cmd = IncludeLaunchDescription(
